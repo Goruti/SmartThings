270c76f0b2eef7c2a9c2edcab717a0e8984973a5
Diego Antonino <diego_antonino@hotmail.com>
2017-08-11 21:31:10 -0400

    adding start process to install.sh

--- SmartThings-Raspberry-TV_integration-master/python_code/install.sh
+++ SmartThings-Raspberry-TV_integration-master/python_code/install.sh
@@ -11,3 +11,9 @@ sudo sed -i -e '$i sleep 20; /bin/ping 192.168.1.252 > /dev/null 2>&1 &\n' /etc/
 echo "Please set you passwords in \
 '/home/pi/git/SmartThings/SmartThings-Raspberry-TV_integration-master/python_code/conf.py' file"
 
+echo "Starting Flask Server"
+python /home/pi/git/SmartThings/SmartThings-Raspberry-TV_integration-master/python_code/flask_restful_server.py  2>&1 | logger &
+echo "Starting ping to ST hub"
+/bin/ping 192.168.1.252 > /dev/null 2>&1 &
+
+
