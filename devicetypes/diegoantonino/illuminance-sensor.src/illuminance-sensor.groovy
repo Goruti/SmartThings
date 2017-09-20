dfbbc9eb01a90793799634aed95611c693d669ee
Diego <diego_antonino@hotmail.com>
2017-09-19 20:07:27 -0400

    Illuminance sensor

--- devicetypes/diegoantonino/illuminance-sensor.src/illuminance-sensor.groovy
+++ devicetypes/diegoantonino/illuminance-sensor.src/illuminance-sensor.groovy
@@ -1,5 +1,5 @@
 /**
- *  Illuminant Sensor TSL2561 connected to a PyCom (it used the RPI to check if it's online or not)
+ *  Illuminance Sensor TSL2561 connected to a PyCom (it used the RPI to check if it's online or not)
  *
  *  Copyright 2017 Diego Antonino
  *
@@ -15,53 +15,54 @@
  */
 
 preferences {
-    input("pycom_mac", "string", title:"PyCom MAC Address", required: true, displayDuringSetup: true)
-    input("pycom_ip", "string", title:"PyCom IP Address", required: false, displayDuringSetup: true)
-    input("raspberry_ip", "string", title:"Raspberry IP Address", required: false, displayDuringSetup: true)
-    input("raspberry_port", "string", title:"Raspberry Port", required: false, displayDuringSetup: true)
-    input("username", "string", title:"Username", required: false, displayDuringSetup: true)
-    input("password", "password", title:"Password", required: false, displayDuringSetup: true)
+    input("pycom_mac", "string", title: "PyCom-mac", required: true, displayDuringSetup: true)
+    input("pycom_ip", "string", title: "PyCom-ip", required: false, displayDuringSetup: true)
+    input("raspberry_ip", "string", title: "Raspberry-ip", required: false, displayDuringSetup: true)
+    input("raspberry_port", "string", title: "Raspberry-port", required: false, displayDuringSetup: true)
+    input("username", "string", title: "Username", required: false, displayDuringSetup: true)
+    input("password", "password", title: "Password", required: false, displayDuringSetup: true)
 }
 
 metadata {
     definition (name: "Illuminance Sensor", namespace: "DiegoAntonino", author: "Diego Antonino") {
         capability "illuminanceMeasurement"
+        capability "Refresh"
 
         command "refresh"
 
-        attribute "hubInfo", "enum", ["online", "offline"]
+        attribute "PyComInfo", "enum", ["online", "offline"]
         attribute "lux", "number"
         attribute "ir_light", "number"
         attribute "total_light", "number"
         attribute "visible_light", "number"
     }
 
-
     simulator {
         // TODO: define status and reply messages here
     }
 
     tiles(scale: 2) {
-        standardTile("PyComInfo", "device.PyComInfo", width: 6, height: 4) {
-            state "online", label:'${name}', action:"refresh", backgroundColor: "#79b821", icon: "st.Electronics.electronics1"
-            state "offline", label:'${name}', action:"refresh",  backgroundColor: "#79b821", icon: "st.Electronics.electronics1"
+        standardTile("PyComInfo", "device.PyComInfo", width: 6, height: 2) {
+            state "online", label:'${name}', action:"refresh", backgroundColor: "#44b621", icon: "st.Electronics.electronics1"
+            state "offline", label:'${name}', action:"refresh",  backgroundColor: "#bc2323", icon: "st.Electronics.electronics1"
         }
-        valueTile("lux", "device.lux", decoration: "flat", width: 6, height: 2) {
+        valueTile("lux", "device.lux", decoration: "flat", width: 6, height: 1) {
             state "lux", label:'${currentValue} Lux'
         }
-        valueTile("ir_light", "device.ir_light", decoration: "flat", width: 2, height: 2) {
-            state "ir_light", label:'${currentValue}'
+        valueTile("ir_light", "device.ir_light", decoration: "flat", width: 2, height: 1) {
+            state "ir_light", label:'${currentValue}\nIR'
         }
-        valueTile("total_light", "device.total_light", decoration: "flat", width: 2, height: 2) {
-            state "total_light", label:'${currentValue}'
+        valueTile("total_light", "device.total_light", decoration: "flat", width: 2, height: 1) {
+            state "total_light", label:'${currentValue}\nTotal'
         }
-        valueTile("visible_light", "device.visible_light", decoration: "flat", width: 2, height: 2) {
-            state "visible_light", label:'${currentValue}'
+        valueTile("visible_light", "device.visible_light", decoration: "flat", width: 2, height: 1) {
+            state "visible_light", label:'${currentValue}\nVisible'
         }
 
         main("PycomInfo")
-        details (["PyComInfo",
-        "lux", "ir_light", "total_light", "visible_light"])
+        details(["PyComInfo",
+        "lux", 
+        "ir_light", "total_light", "visible_light"])
     }
 }
 
@@ -89,27 +90,26 @@ def parse(String description) {
         sendEvent(name: "ir_light", value: "${body.ir_light}")
         sendEvent(name: "total_light", value: "${body.total_light}")
         sendEvent(name: "visible_light", value: "${body.visible_light}")
-
     }
 }
 
-//def refresh() {
-//    log.debug "<Device Handler> Checking State"
-//    def userpass = encodeCredentials(username, password)
-//    def headers = getHeader(userpass)
-//
-//    def hubAction = new physicalgraph.device.HubAction(
-//            method: "GET",
-//            path: "/homesweethome/api/v1.0/device/status/${pycom_ip}",
-//            headers: headers,
-//    )
-//
-//    try {
-//        return sendHubCommand(hubAction)
-//    } catch (Exception e) {
-//        log.debug "Hit Exception ${e} on ${hubAction}"
-//    }
-//}
+def refresh() {
+    log.debug "<Device Handler> Checking State"
+    def userpass = encodeCredentials(username, password)
+    def headers = getHeader(userpass)
+
+    def hubAction = new physicalgraph.device.HubAction(
+            method: "GET",
+            path: "/homesweethome/api/v1.0/device/status/${pycom_ip}",
+            headers: headers,
+    )
+
+    try {
+        return sendHubCommand(hubAction)
+    } catch (Exception e) {
+        log.debug "Hit Exception ${e} on ${hubAction}"
+    }
+}
 
 
 // ------------------------------------------------------------------
