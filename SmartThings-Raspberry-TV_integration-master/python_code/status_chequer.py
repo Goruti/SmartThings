e617c9c067740ce3c233b586c56e8c9652ae14e4
Diego Antonino <dantonino@mnubo.com>
2017-10-24 20:56:04 -0400

    minor fixes

--- SmartThings-Raspberry-TV_integration-master/python_code/status_chequer.py
+++ SmartThings-Raspberry-TV_integration-master/python_code/status_chequer.py
@@ -5,15 +5,19 @@ import time
 
 
 def main():
+    print "Stating 'Status Chequer'"
     TV_IP = conf.TV_IP
     TV_STATUS = ''
 
-    while True:
-        answer = json.loads(flask_restful_server.check_status(TV_IP))
-        if TV_STATUS != answer.get("status"):
-            TV_STATUS = answer.get("status")
-            flask_restful_server.send_event(json.dumps(answer))
-        time.sleep(60)
+    try:
+        while True:
+            answer = json.loads(flask_restful_server.check_status(TV_IP))
+            if TV_STATUS != answer.get("status"):
+                TV_STATUS = answer.get("status")
+                flask_restful_server.send_event(json.dumps(answer))
+            time.sleep(60)
+    except Exception as e:
+        print "Error in Status Chequer. Error:".format(e)
 
 
 if __name__ == '__main__':
