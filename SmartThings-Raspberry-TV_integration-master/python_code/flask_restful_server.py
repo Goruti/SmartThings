6a1ebea15c672726c8fa3ac78170c6c3f9657a99
Diego Antonino <dantonino@mnubo.com>
2017-12-28 12:03:38 -0500

    adding get_tv_ip

--- SmartThings-Raspberry-TV_integration-master/python_code/flask_restful_server.py
+++ SmartThings-Raspberry-TV_integration-master/python_code/flask_restful_server.py
@@ -10,6 +10,10 @@ from flask_httpauth import HTTPBasicAuth
 import lg_API
 from python_common_tools.tools import get_st_ip
 from conf import configuration
+import ssdp
+import requests
+from urlparse import urlparse
+import xml.etree.ElementTree as ET
 
 
 auth = HTTPBasicAuth()
@@ -131,16 +135,47 @@ def select_tv_input(tv_ip):
     err_code, message = lg_API.ControlAction(tv_ip, "20")
 
 
-if __name__ == '__main__':
-    ST_IP = get_st_ip()
-
-    if not ST_IP:
-        print "Smartthings Hub is not UP"
+def get_smartthing_ip():
+    st_ip = get_st_ip()
+    if not st_ip:
+        print "Smartthings Hub is DOWN"
         exit(1)
-    configuration["ST_IP"] = ST_IP
-    with open('conf.py', 'w') as f:
-        f.write("configuration = {}".format(configuration))
-    time.sleep(1)
+
+    if configuration["ST_IP"] != st_ip:
+        configuration["ST_IP"] = st_ip
+        with open('conf.py', 'w') as f:
+            f.write("configuration = {}".format(configuration))
+        time.sleep(1)
+
+
+def get_tv_ip():
+    tv_search_query = "schemas-upnp-org:service:AVTransport:1"
+    for device in ssdp.discover(tv_search_query):
+        location = device.location
+        device = ET.fromstring(requests.get(location).text)[1]
+
+        if len(device) >= 10:
+            if device[4].text == 'LG Digital Media Renderer TV' and \
+                            device[7].text == 'LG Electronics' and \
+                            device[10].text == 'LG TV':
+                tv_ip = urlparse(location).netloc.split(":")[0]
+
+    if not tv_ip:
+        print "TV is OFF, please turn on your LG TV"
+        exit(1)
+
+    if configuration["TV_IP"] != tv_ip:
+        configuration["TV_IP"] = tv_ip
+        with open('conf.py', 'w') as f:
+            f.write("configuration = {}".format(configuration))
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    #  GET ST IP
+    get_smartthing_ip()
+    #  GET TV IP
+    get_tv_ip()
 
     os.system("python /home/pi/git/SmartThings/SmartThings-Raspberry-TV_integration-master/python_code/full_status_chequer.py 2>&1 | logger &")
     app.run(host='0.0.0.0', port=5000, debug=True)
