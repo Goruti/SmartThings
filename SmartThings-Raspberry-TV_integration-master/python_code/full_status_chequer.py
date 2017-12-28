03c5fc907c54616eb4368ef09a20e2900f675605
Diego Antonino <dantonino@mnubo.com>
2017-12-28 15:39:33 -0500

    moving things around

--- SmartThings-Raspberry-TV_integration-master/python_code/full_status_chequer.py
+++ SmartThings-Raspberry-TV_integration-master/python_code/full_status_chequer.py
@@ -1,10 +1,7 @@
 from conf import configuration
 import json
 import time
-import flask_restful_server
-
-from python_common_tools.tools import send_event_to_st
-from python_common_tools.tools import get_send_rpi_stats
+import tools
 
 
 def main():
@@ -15,7 +12,7 @@ def main():
             #  check TV status
             TV_STATUS = check_tv_status(TV_STATUS)
             #  check RPI status
-            send_event_to_st(json.dumps(get_send_rpi_stats()), configuration["ST_IP"])
+            tools.send_event_to_st(json.dumps(tools.get_send_rpi_stats()), configuration["ST_IP"])
             time.sleep(configuration["SLEEP_TIME"])
     except Exception as e:
         print "Error in Status Chequer. Error:".format(e)
@@ -23,11 +20,11 @@ def main():
 
 def check_tv_status(status):
     tv_ip = configuration["TV_IP"]
-    resp = json.loads(flask_restful_server.check_status(tv_ip))
+    resp = json.loads(tools.check_status(tv_ip))
     if status != resp.get("status"):
         status = resp.get("status")
         answer = {"type": "tv_status", "body": resp}
-        send_event_to_st(json.dumps(answer), configuration["ST_IP"])
+        tools.send_event_to_st(json.dumps(answer), configuration["ST_IP"])
         return status
 
 
